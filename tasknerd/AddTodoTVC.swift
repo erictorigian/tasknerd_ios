653b8d5d3c4748684b1097829b25b3217f0ec84c
Eric Torigian <eric@erics-air.hsd1.mi.comcast.net>
2016-08-07 00:53:29 -0400

    add todo working and stay logged in

--- tasknerd/AddTodoTVC.swift
+++ tasknerd/AddTodoTVC.swift
@@ -7,13 +7,35 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
 
 class AddTodoTVC: UITableViewController {
 
+    let networkingService = NetworkingServices()
+    
+    @IBOutlet weak var todoDetails: UITextView!
+    @IBOutlet weak var todoName: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
       
+    }
+    @IBAction func saveTodo(_ sender: AnyObject) {
+        let todoRef = self.networkingService.databaseRef.child("todos").childByAutoId()
+        
+        if let name = todoName.text, name != "", let details = todoDetails.text, details != "" {
+            var todo = Todo(todoName: name, todoDetails: details, username: (FIRAuth.auth()?.currentUser?.displayName)!)
+            todoRef.setValue(todo.toAnyObject())
+        }
+        
+        
+        
+
     }
 
    }
---
+++ tasknerd/Todo.swift
@@ -0,0 +1,48 @@
+//
+//  Todo.swift
+//  tasknerd
+//
+//  Created by Eric Torigian on 8/6/16.
+//  Copyright © 2016 Eric Torigian. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseDatabase
+
+struct Todo {
+    var todoName: String!
+    var todoDetails: String!
+    var todoComplete: String!
+    var username: String!
+    var ref: FIRDatabaseReference?
+    var key: String!
+
+    
+    init(todoName: String, todoDetails: String, username: String) {
+        self.todoComplete = "false"
+        self.todoName = todoName
+        self.todoDetails = todoDetails
+        self.username = username
+        self.key = ""
+        self.ref = FIRDatabase.database().reference()
+    }
+    
+    init(snapshot: FIRDataSnapshot) {
+        self.todoComplete = snapshot.value!["todoComplete"] as! String
+        self.todoName = snapshot.value!["todoName"] as! String
+        self.todoDetails = snapshot.value!["todoDetails"] as! String
+        self.username = snapshot.value!["username"] as! String
+        self.key = snapshot.key
+        self.ref = snapshot.ref
+        
+    }
+    
+    mutating func toAnyObject() -> [String: AnyObject] {
+        var todoDict: [String: AnyObject]
+        todoDict = ["todoName": todoName, "todoDetails": todoDetails,
+                    "todoComplete": todoComplete, "username": username ]
+        
+        return todoDict
+    }
+}
